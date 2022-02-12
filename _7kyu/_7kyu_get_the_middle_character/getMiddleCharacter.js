2d7e5b670ff0428863d2c8ab89b176affc726a83
Syazwan Fitri <syazfitry69@gmail.com>
2022-02-12 13:36:42 +0800

    Added solution for _7kyu_get_the_middle_character

---
+++ _7kyu/_7kyu_get_the_middle_character/getMiddleCharacter.js
@@ -0,0 +1,22 @@
+function getMiddle(s)
+{
+  let arrS = s.split('');
+  let arrStwo;
+  let midWords;
+  console.log(arrS);
+  
+  if(arrS.length === 1) {
+    midWords = arrS.join('');
+    return midWords;
+  }
+  
+  if((arrS.length % 2) !== 0) {
+    midWords = arrS[Math.floor(arrS.length / 2)];
+    console.log(arrS.length / 2);
+  } else {
+    let n = (arrS.length / 2)
+    arrStwo = arrS.splice(n);
+    midWords = arrS.pop() + arrStwo.shift();
+  }
+  return midWords; 
+}
