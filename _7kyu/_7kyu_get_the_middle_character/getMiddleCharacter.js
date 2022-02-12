a2bec78aa12ad277972f448870f3bf31530e6531
Syazwan Fitri <syazfitry69@gmail.com>
2022-02-12 13:44:32 +0800

    solution

--- _7kyu/_7kyu_get_the_middle_character/getMiddleCharacter.js
+++ _7kyu/_7kyu_get_the_middle_character/getMiddleCharacter.js
@@ -3,7 +3,6 @@ function getMiddle(s)
   let arrS = s.split('');
   let arrStwo;
   let midWords;
-  console.log(arrS);
   
   if(arrS.length === 1) {
     midWords = arrS.join('');
@@ -12,7 +11,6 @@ function getMiddle(s)
   
   if((arrS.length % 2) !== 0) {
     midWords = arrS[Math.floor(arrS.length / 2)];
-    console.log(arrS.length / 2);
   } else {
     let n = (arrS.length / 2)
     arrStwo = arrS.splice(n);
@@ -20,3 +18,6 @@ function getMiddle(s)
   }
   return midWords; 
 }
+
+
+
