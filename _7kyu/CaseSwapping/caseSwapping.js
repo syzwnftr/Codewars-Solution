b722a785eb44ae88f5f00a6e3164b68b5bec059c
Syazwan Fitri <syazfitry69@gmail.com>
2022-03-23 11:28:10 +0800

    Added solution for Case Swapping

---
+++ _7kyu/CaseSwapping/caseSwapping.js
@@ -0,0 +1,19 @@
+/*
+
+Given a string, swap the case for each of the letters.
+
+e.g. CodEwArs --> cODeWaRS
+Examples
+
+""           ->   ""
+"CodeWars"   ->   "cODEwARS"
+"abc"        ->   "ABC"
+"ABC"        ->   "abc"
+"123235"     ->   "123235"
+
+
+*/
+
+function swap(str){
+    return str.split('').map(letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()).join('')
+  }
