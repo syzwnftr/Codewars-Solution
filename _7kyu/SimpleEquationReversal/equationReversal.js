8173383f2d2915c675fb4f91a9b816cd0f169ed2
Syazwan Fitri <syazfitry69@gmail.com>
2022-04-10 16:24:57 +0800

    Added comments and some changes

--- _7kyu/SimpleEquationReversal/equationReversal.js
+++ _7kyu/SimpleEquationReversal/equationReversal.js
@@ -13,10 +13,14 @@ Good luck!
 
 
 function solve(eq) {
+    // replace number with 'N', split the string into an array and reverse it
     let result = eq.replace(/[0-9]+/g, 'N').split('').reverse();
-    let num = eq.split(/\D+/).filter(num => num != '').reverse();
 
-     for(let i = 0; i < result.length; i++) {
+    // split str into an array (only number) and reverse it
+    let num = eq.split(/[^0-9]+/).filter(num => num != '').reverse();
+
+    // replace every 'N' in result with number
+    for(let i = 0; i < result.length; i++) {
         if(result[i] === 'N') {
             result[i] = num.shift();
         }
