6aa80b1354193498f7174fb29b2db5f23ebecc43
Syazwan Fitri <syazfitry69@gmail.com>
2022-04-28 06:28:03 +0800

    Added solution for How Many Arguments

---
+++ _7kyu/HowManyArguments/howManyArgs.js
@@ -0,0 +1,12 @@
+/*
+
+Create a function called args_count that returns the number of arguments provided
+
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+
+*/
+
+function args_count() {
+    return arguments.length;
+}
