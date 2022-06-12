7596c869a13c31b37b045ffd7fd2c28d8a00ebf4
Syazwan Fitri <syazfitry69@gmail.com>
2022-06-12 11:55:12 +0800

    Adds >> ReversedSequence

---
+++ _8kyu/ReversedSequence/reversedSequence.js
@@ -0,0 +1,15 @@
+/*
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+
+*/
+
+const reverseSeq = n => {
+    const result = [];
+    for(let i = 0; i < n; i++) {
+      result.push(n - i);
+    }
+    return result;
+  };
