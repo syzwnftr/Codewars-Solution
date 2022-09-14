adfcac7d88d3708adb1c29ebbb6daa468b6d1056
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-14 16:28:44 +0800

    Added solution >> RemoveAnchorFromURL

---
+++ _7kyu/RemoveAnchorFromURL/removeAnchorURL.js
@@ -0,0 +1,27 @@
+/*
+
+Description:
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+Examples
+
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+*/
+
+function removeUrlAnchor(url){
+    let result = '';
+    for (let i = 0; i < url.length; i++) {
+      if (url[i] === '#') {
+        return result;
+      }
+      result+= url[i];
+    }
+    return result;
+}
+
+// alt:
+function removeUrlAnchor(url){
+    return url.split('#')[0];
+}
