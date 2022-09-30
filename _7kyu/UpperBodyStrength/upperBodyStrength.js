4905afb07ccbdeb7db3c9569430440444d804adb
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-30 17:43:10 +0800

    Added solution >> UpperBodyStrength

---
+++ _7kyu/UpperBodyStrength/upperBodyStrength.js
@@ -0,0 +1,21 @@
+/*
+
+Alex is transitioning from website design to coding and wants to sharpen his skills with CodeWars.
+He can do ten kata in an hour, but when he makes a mistake, he must do pushups. These pushups really tire poor Alex out, so every time he does them they take twice as long. His first set of redemption pushups takes 5 minutes. Create a function, alexMistakes, that takes two arguments: the number of kata he needs to complete, and the time in minutes he has to complete them. Your function should return how many mistakes Alex can afford to make.
+
+*/
+
+function alexMistakes(numberOfKata, timeLimit) {
+  let timeTaken = numberOfKata * 6;
+  let timeRemain = timeLimit - timeTaken;
+  let pushUpTime = 5;
+  let mistake = 0;
+
+  while (timeRemain > pushUpTime) {
+    mistake += 1;
+    timeRemain -= pushUpTime;
+    pushUpTime *= 2;
+  }
+
+  return mistake;
+}
