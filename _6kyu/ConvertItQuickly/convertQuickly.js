3d3a2ae4ebca3f9d932331a78c6a272d7ff15eae
Syazwan Fitri <syazfitry69@gmail.com>
2022-10-14 12:32:19 +0800

    Added solution >> ConvertItQuickly

---
+++ _6kyu/ConvertItQuickly/convertQuickly.js
@@ -0,0 +1,15 @@
+/*
+
+#Input: string containing a single positive valid hex number without # symbol. Ex: 'FF'.
+
+#Output: converted decimal number, 255
+
+#Restriction: code length <= 26 characters
+
+#Trivia: you have time to type only 26 characters before the daily angry zombie mob will break into your house.
+
+If you successfully fought back try to move to the next level
+
+*/
+
+return (x) => parseInt(x, 16);
