7a82579294eeda9b3459fe55d1144b22df04919f
Syazwan Fitri <syazfitry69@gmail.com>
2022-10-15 15:22:06 +0800

    Added solution >> 5WithoutAlphanums

---
+++ _7kyu/5WithoutAlphanums/5WithoutAlphanums.js
@@ -0,0 +1,12 @@
+/*
+
+Write a function/proc/lambda called _ that always returns 5.
+
+Sounds easy right? Just bear in mind that you can only use the following characters:
+
+~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`
+
+*/
+
+// remove spaces in between to make this work
+_ = () => ("" == "") + ("" == "") + ("" == "") + ("" == "") + ("" == "");
